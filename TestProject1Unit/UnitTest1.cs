b2516a7729524eea6b07b5941903e5f4ea6859c9
Nikita Kravchenko <89710564+NicKravchenko@users.noreply.github.com>
2022-10-14 17:09:26 -0400

    Update UnitTest1.cs

--- TestProject1Unit/UnitTest1.cs
+++ TestProject1Unit/UnitTest1.cs
@@ -16,6 +16,8 @@
 //}
 
 using NUnit.Framework;
+using ProjectGolfPapa.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TestProject1Unit
 {
@@ -36,12 +38,30 @@ namespace TestProject1Unit
             Assert.NotZero(result, $"{result} No igual a 0");
         }
 
+        
+       
         [Test]
-        public void InputIs0_ReturnFalse()
+        public void ValoresVacios()
         {
-            //Assert.Pass();
-            var result = 0;
-            Assert.NotZero(result, $"{result} No igual a 0");
+            var Names = new List<string>() { "Firaluais 1", "Firaluais 2", "Firaluais 1", " " };
+            var ListAnimal = new List<String>() { "Pastor Aleman", "Pitbull", "Chihuahua", " " };
+            var GenderName = new List<string>() { "Masculino", "Femenino", " "};
+            var RaceName = new List<string>() { "Raza 1", "Raza 2", "Raza 3", " " };
+
+            var pet = new Pet()
+            {
+                Name = Names[new Random().Next(0, 4)],
+                Animal = ListAnimal[new Random().Next(0,4)],
+                Gender = GenderName[new Random().Next(0,3)],
+                Race =  RaceName[new Random().Next(0,4)],
+            };
+
+            Assert.AreNotEqual( " ", pet.Name);
+            Assert.AreNotEqual(" ", pet.Animal);
+            Assert.AreNotEqual(" ", pet.Gender);
+            Assert.AreNotEqual(" ", pet.Race);
+
+
         }
     }
 }
